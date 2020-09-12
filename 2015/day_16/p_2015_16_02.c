7b2d78a6107056de5d6d7bb9b10c0457cb2e4f61
Kevin Colour <kecolour@gmail.com>
2020-09-11 23:29:45 -0700

    Day 16 p2

--- 2015/day_16/p_2015_16_02.c
+++ 2015/day_16/p_2015_16_02.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   p_2015_16_01.c                                     :+:      :+:    :+:   */
+/*   p_2015_16_02.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: home <home@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 23:03:23 by home              #+#    #+#             */
-/*   Updated: 2020/09/11 23:08:25 by home             ###   ########.fr       */
+/*   Updated: 2020/09/11 23:29:14 by home             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,18 +48,20 @@ int	main(void)
 	int		j;
 	bool	match;
 	char	*ptr;
+	char	*num;
+	int		spec;
 	char	*s_tok;
 	char	*str_file;
 	char	*(good_aunt_sue[11]);
 
 	good_aunt_sue[0] = " children: 3";
-	good_aunt_sue[1] = " cats: 7";
+	good_aunt_sue[1] = " cats: b";
 	good_aunt_sue[2] = " samoyeds: 2";
-	good_aunt_sue[3] = " pomeranians: 3";
+	good_aunt_sue[3] = " pomeranians: b";
 	good_aunt_sue[4] = " akitas: 0";
 	good_aunt_sue[5] = " vizslas: 0";
-	good_aunt_sue[6] = " goldfish: 5";
-	good_aunt_sue[7] = " trees: 3";
+	good_aunt_sue[6] = " goldfish: b";
+	good_aunt_sue[7] = " trees: b";
 	good_aunt_sue[8] = " cars: 2";
 	good_aunt_sue[9] = " perfumes: 1";
 	good_aunt_sue[10] = NULL;
@@ -80,6 +82,18 @@ int	main(void)
 			while (good_aunt_sue[j] != NULL)
 			{
 				if (strncmp(ptr, good_aunt_sue[j], strlen(good_aunt_sue[j])) == 0)
+					break ;
+				num = strpbrk(ptr, "1234567890-");
+				spec = atoi(num);
+
+				if (strncmp(ptr, " cats", strlen(" cats")) == 0 && spec > 7)
+					break ;
+				if (strncmp(ptr, " trees", strlen(" trees")) == 0 && spec > 3)
+					break ;
+
+				if (strncmp(ptr, " pomeranians", strlen(" pomeranians")) == 0 && spec < 3)
+					break ;
+				if (strncmp(ptr, " goldfish", strlen(" goldfish")) == 0 && spec < 5)
 					break ;
 				j++;
 			}
