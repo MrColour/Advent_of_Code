e32f55fa0b55ab55c5a12546788b94641b254aba
Kevin Colour <kecolour@gmail.com>
2020-09-14 22:08:17 -0700

    Press SHIFT to dash

--- 2016/day_01/p_2016_01_01.c
+++ 2016/day_01/p_2016_01_01.c
@@ -6,7 +6,7 @@
 /*   By: home <home@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 16:19:56 by home              #+#    #+#             */
-/*   Updated: 2020/09/13 22:53:05 by home             ###   ########.fr       */
+/*   Updated: 2020/09/14 19:33:09 by home             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,50 @@
 int		main(void)
 {
 	char	*str_file;
+	char	*s_tok;
+	int		dir;
+	int		x;
+	int		y;
+	int		steps;
 
 	str_file = extract_file("input.txt");
+	s_tok = strtok(str_file, " ");
 
+	dir = 0;
+	x = 0;
+	y = 0;
+	while (s_tok != NULL)
+	{
+		steps = atoi(&(s_tok[1]));
+
+		if (s_tok[0] == 'R')
+			dir++;
+		else
+			dir--;
+
+		if (dir == -1)
+			dir = 3;
+
+		if (dir == 4)
+			dir = 0;
+
+		if (dir == 0)
+			y += steps;
+		if (dir == 2)
+			y -= steps;
+
+		if (dir == 1)
+			x += steps;
+		if (dir == 3)
+			x -= steps;
+
+		printf("%s with %d\n", s_tok, steps);
+		s_tok = strtok(NULL, " ");
+	}
+	if (x <= 0)
+		x *= -1;
+	if (y <= 0)
+		y *= -1;
+	printf("%d\n", (y + x));
 	return (0);
 }
---
+++ 2016/day_01/p_2016_01_02.c
@@ -0,0 +1,172 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   p_2016_01_02.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: home <home@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/14 19:35:28 by home              #+#    #+#             */
+/*   Updated: 2020/09/14 20:57:07 by home             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../aoc++.h"
+
+typedef	struct s_location
+{
+	int		x;
+	int		y;
+
+	int		p_x;
+	int		p_y;
+}		t_location;
+
+bool	intersect(t_location a, t_location b)
+{
+	bool	result;
+	int		temp;
+
+	result = false;
+	if (a.p_x > a.x)
+	{
+		temp = a.x;
+		a.x = a.p_x;
+		a.p_x = temp;
+	}
+
+	if (a.p_y > a.y)
+	{
+		temp = a.y;
+		a.y = a.p_y;
+		a.p_y = temp;
+	}
+
+	if (b.p_x > b.x)
+	{
+		temp = b.x;
+		b.x = b.p_x;
+		b.p_x = temp;
+	}
+
+	if (b.p_y > b.y)
+	{
+		temp = b.y;
+		b.y = b.p_y;
+		b.p_y = temp;
+	}
+
+	if (a.p_y == a.y)
+	{
+		if (bound(b.p_y, a.y, b.y) && bound(a.p_x, b.x, a.x))
+		{
+			printf("here\n");
+			result = true;
+		}
+		// if (b.y == b.p_y && (bound(a.p_x, b.x, a.x) || bound(a.p_x, b.p_x, a.x)))
+		// 	result = true;
+	}
+
+	if (a.p_x == a.x)
+	{
+		if (bound(b.p_x, a.x, b.x) && bound(a.p_y, b.y, a.y))
+		{
+			printf("her\n");
+			result = true;
+		}
+		// if (b.x == b.p_x && (bound(a.p_y, b.y, a.y) || bound(a.p_y, b.p_y, a.y)))
+		// 	result = true;
+	}
+	return (result);
+}
+
+int		main(void)
+{
+	char	*str_file;
+	char	*s_tok;
+	int		i;
+	int		dir;
+	int		steps;
+	int		x;
+	int		y;
+	int		locations;
+	t_location	*visit;
+
+	str_file = extract_file("input.txt");
+
+	i = 0;
+	x = 0;
+	y = 0;
+	dir = 0;
+	locations = count_occur(" ", str_file);
+	s_tok = strtok(str_file, " ");
+	visit = calloc(locations + 1, sizeof(*visit));
+	visit[0].p_x = 0;
+	visit[0].p_y = 0;
+	while (s_tok != NULL)
+	{
+		steps = atoi(&(s_tok[1]));
+
+		if (s_tok[0] == 'R')
+			dir++;
+		else
+			dir--;
+
+
+		if (dir == -1)
+			dir = 3;
+
+		if (dir == 4)
+			dir = 0;
+
+
+		if (dir == 0)
+			y += steps;
+		if (dir == 2)
+			y -= steps;
+
+		if (dir == 1)
+			x += steps;
+		if (dir == 3)
+			x -= steps;
+
+		visit[i].x = x;
+		visit[i].y = y;
+
+		visit[i + 1].p_x = x;
+		visit[i + 1].p_y = y;
+
+		// printf("(%d, %d) %d\n", visit[i].x, visit[i].y, i);
+		// printf("%s with %d\n", s_tok, steps);
+		s_tok = strtok(NULL, " ");
+		i++;
+	}
+	printf("SIZE: %d\n", i);
+
+	int	size;
+	int	j;
+
+	size = i;
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		x = visit[i].x;
+		y = visit[i].y;
+		printf("(%d, %d) %d\n", visit[i].x, visit[i].y, i);
+		while (j < i)
+		{
+			if (intersect(visit[i], visit[j]) == true)
+				break ;
+			j++;
+		}
+		if (i != j)
+			break ;
+		i++;
+	}
+	printf("(%d, %d) to (%d, %d) intersect (%d, %d) to (%d, %d)\n",
+		visit[i].p_x, visit[i].p_y, visit[i].x, visit[i].y,
+		visit[j].p_x, visit[j].p_y, visit[j].x, visit[j].y);
+
+	printf("%d\n", (119 + 14));
+	return (0);
+}
