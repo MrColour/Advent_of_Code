b7dd07ccf2eb244ffc0068e8e08d2e9cb765e62c
Kevin Colour <kecolour@gmail.com>
2020-09-16 00:38:03 -0700

    Better Asserts

--- spawn.c
+++ spawn.c
@@ -6,9 +6,15 @@
 /*   By: home <home@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 17:53:28 by home              #+#    #+#             */
-/*   Updated: 2020/09/16 00:20:11 by home             ###   ########.fr       */
+/*   Updated: 2020/09/16 00:36:56 by home             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+* USAGE:go into the year you are working at and run the compiled file as
+* `./a.out YEAR DAY` where YEAR is a number between 2015 and 2019 and
+* DAY is a number between 1 or 25.
+*/
 
 #include <stdio.h> // asprintf
 #include <assert.h> //assert
@@ -26,8 +32,8 @@ void	make_day(char *s_year, char *s_day)
 	day = atoi(s_day);
 	year = atoi(s_year);
 
-	assert(0 < day && day <= 25);
-	assert(2015 < year && year <= 2019);
+	assert(0 < day && day <= 25 && "Day argument is out of bounds. Valid range is [1, 25].");
+	assert(2015 < year && year <= 2019 && "Year argument is out of bounds. Valid range is [2015, 2019]");
 
 	asprintf(&day_dir, "day_%02d", day);
 	mkdir(day_dir, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
@@ -53,7 +59,7 @@ void	make_day(char *s_year, char *s_day)
 
 int		main(int aa, char **args)
 {
-	assert(aa == 3);
+	assert(aa == 3 && "Insufficient Arguments provided. Please supply a Year and a Day.");
 	make_day(args[1], args[2]);
 	return (0);
 }
