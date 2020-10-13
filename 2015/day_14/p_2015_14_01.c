add7a800043b5229c7cef2ae21de9409cf00a386
Kevin Colour <kecolour@gmail.com>
2020-10-13 03:19:48 -0700

    Refactor

--- 2015/day_14/p_2015_14_01.c
+++ 2015/day_14/p_2015_14_01.c
@@ -6,7 +6,7 @@
 /*   By: home <home@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/04 23:30:05 by home              #+#    #+#             */
-/*   Updated: 2020/09/28 16:06:39 by home             ###   ########.fr       */
+/*   Updated: 2020/10/13 02:46:12 by home             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,6 @@ int		calc_reindeer_dist(int speed, int fly_dur, int rest_dur, int race_dur)
 int		main(void)
 {
 	char	*str_file;
-	char	*ptr;
 	int		race_dur;
 	int		speed, fly_dur, rest_dur;
 
@@ -45,19 +44,15 @@ int		main(void)
 
 	race_dur = 2503;
 	str_file = extract_file("input.txt");
-	str_file = strtok(str_file, "\n");
-	while (str_file != NULL)
-	{
-		ptr = str_file;
-		ptr += fetch_int(ptr, &speed);
-		ptr += fetch_int(ptr, &fly_dur);
-		ptr += fetch_int(ptr, &rest_dur);
+
+	FOR_EACH_STRTOK(str_file, "\n",
+		_tok += fetch_int(_tok, &speed);
+		_tok += fetch_int(_tok, &fly_dur);
+		_tok += fetch_int(_tok, &rest_dur);
 
 		temp = calc_reindeer_dist(speed, fly_dur, rest_dur, race_dur);
 		max = MAX(temp, max);
-
-		str_file = strtok(NULL, "\n");
-	}
-	printf("Result: %d\n", max);
+	)
+	answer(d, max);
 	return (0);
 }
--- 2015/day_14/p_2015_14_02.c
+++ 2015/day_14/p_2015_14_02.c
@@ -6,7 +6,7 @@
 /*   By: home <home@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/05 01:35:52 by home              #+#    #+#             */
-/*   Updated: 2020/09/28 20:56:22 by home             ###   ########.fr       */
+/*   Updated: 2020/10/13 03:18:13 by home             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 typedef	struct	s_reindeer
 {
+	int			points; //Using the fact that this is the first item in the struct for a sort search. Beware if moved.
+
 	int			speed;
 	int			fly_dur;
 	int			rest_dur;
 
 	int			cur_dist;
-	int			points;
 
 	int			fly_t_rem;
 	int			rest_t_rem;
@@ -28,40 +29,30 @@ typedef	struct	s_reindeer
 int		main(void)
 {
 	char	*str_file;
-	char	*ptr;
-	int		i;
 	int		reindeer_count;
 	t_reindeer	*reindeer_array;
 
 	str_file = extract_file("input.txt");
 	reindeer_count = count_occur("\n", str_file);
-
-	i = 0;
-	str_file = strtok(str_file, "\n");
 	reindeer_array = calloc(reindeer_count, sizeof(*reindeer_array));
-	while (str_file != NULL)
-	{
-		ptr = str_file;
 
-		ptr += fetch_int(ptr, &(reindeer_array[i].speed));
-		ptr += fetch_int(ptr, &(reindeer_array[i].fly_dur));
-		ptr += fetch_int(ptr, &(reindeer_array[i].rest_dur));
+	FOR_EACH_STRTOK(str_file, "\n",
+		_tok += fetch_int(_tok, &(reindeer_array[_i].speed));
+		_tok += fetch_int(_tok, &(reindeer_array[_i].fly_dur));
+		_tok += fetch_int(_tok, &(reindeer_array[_i].rest_dur));
 
-		reindeer_array[i].fly_t_rem = reindeer_array[i].fly_dur;
-		reindeer_array[i].rest_t_rem = reindeer_array[i].rest_dur;
+		reindeer_array[_i].fly_t_rem = reindeer_array[_i].fly_dur;
+		reindeer_array[_i].rest_t_rem = reindeer_array[_i].rest_dur;
+	)
 
-		str_file = strtok(NULL, "\n");
-		i++;
-	}
-
-	int		race_time;
+	int		i;
 	int		lead_dis;
+	int		race_time;
 
 	lead_dis = 0;
 	race_time = 2503;
 	while (race_time > 0)
 	{
-
 		i = 0;
 		while (i < reindeer_count)
 		{
@@ -83,28 +74,18 @@ int		main(void)
 			i++;
 		}
 
-		// printf("LEAD: %d %d\n", reindeer_array[0].cur_dist, reindeer_array[1].cur_dist);
-		i = 0;
-		while (i < reindeer_count)
-		{
-			if (reindeer_array[i].cur_dist == lead_dis)
-				reindeer_array[i].points++;
-			i++;
-		}
+		FOR_EACH(_i < reindeer_count,
+			if (reindeer_array[_i].cur_dist == lead_dis)
+				reindeer_array[_i].points++;
+		)
 
 		race_time--;
 	}
 
-	int max_points;
+	int *max_points;
 
-	i = 0;
-	max_points = 0;
-	while (i < reindeer_count)
-	{
-		// printf("%d\n", reindeer_array[i].points);
-		max_points = MAX(reindeer_array[i].points, max_points);
-		i++;
-	}
-	printf("Result: %d\n", max_points);
+	max_points = last(reindeer_array, reindeer_count, sizeof(*reindeer_array), int_cmp_asc); //Assumes points is first mem of struct
+
+	answer(d, *max_points);
 	return (0);
 }
