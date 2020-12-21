cd1eb205642941b9e8cba53e7407120dfe16d30f
Kevin Colour <kecolour@gmail.com>
2020-12-20 22:01:12 -0800

    Day 21

---
+++ 2020/day_21/p_2020_21_01.py
@@ -0,0 +1,57 @@
+import re
+from collections import defaultdict
+
+input = open("input.txt").read().split("\n")
+
+collection = input
+
+# mem = defaultdict(list)
+
+all_name = defaultdict(list)
+count = 0
+all_i = []
+for i, item in enumerate(collection):
+
+	# print(item)
+	ingr, conts = item.replace(')', '').split("(contains ")
+	conts = conts.split(', ')
+	ingr = ingr.strip()
+	# print(conts)
+	# print(ingr)
+	all_i += ingr.split(' ')
+	for allergen in conts:
+		if (allergen not in all_name):
+			# print(allergen, 'got added', ingr.split(' '))
+			all_name[allergen] = [set(ingr.split(' '))]
+		else:
+			# print(allergen, 'got added', ingr.split(' '))
+			all_name[allergen].append(set(ingr.split(' ')))
+
+# print(all_name)
+
+print(all_name['fish'])
+
+ban = []
+
+for allergen in all_name:
+	left_over = set(all_name[allergen][0])
+	# print("Set", left_over)
+
+	for s in all_name[allergen]:
+		# print("Cross", s)
+		left_over = left_over.intersection(s)
+
+	print(allergen, left_over)
+	ban += left_over
+
+print(ban)
+
+# print(all_i)
+
+count = 0
+for i in all_i:
+	if (i not in ban):
+		count += 1
+		# print("Good", i)
+
+print(count)
---
+++ 2020/day_21/p_2020_21_02.py
@@ -0,0 +1,14 @@
+# Part one gave me:
+
+# fish {'rkkrx', 'bmrmhm'}
+# peanuts {'vflms', 'bmrmhm'}
+# nuts {'bqkndvb', 'bmrmhm', 'snhrpv'}
+# eggs {'zmb', 'snhrpv'}
+# dairy {'vflms', 'bqkndvb'}
+# wheat {'rkkrx'}
+# soy {'rkkrx', 'qzkjrtl', 'bmrmhm'}
+
+# After rearranging them in alpha order
+# With the use of some vscode shortcuts I was able to figure out what remains
+
+# bqkndvb,zmb,bmrmhm,snhrpv,vflms,bqtvr,qzkjrtl,rkkrx
